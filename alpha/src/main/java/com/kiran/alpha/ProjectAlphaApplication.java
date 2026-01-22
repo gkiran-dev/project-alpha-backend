87abbe664fef7f1d2bf2194ad8b1891b1a2d6376
GKiran <gkiran.dev5@gmail.com>
2026-01-22 12:50:00 +0530

    Project Alpha with Spring boot and Hibernate

---
+++ alpha/src/main/java/com/kiran/alpha/ProjectAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAlphaApplication.class, args);
+	}
+
+}
---
+++ alpha/src/test/java/com/kiran/alpha/ProjectAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
